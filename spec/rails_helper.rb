0c5d66dee7d139b98af23b55dd5a59097de7fa5a
Nathan Burgess <nathan@3playmedia.com>
2018-04-27 08:04:58 -0400

    Model associations/validations and specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,7 +27,7 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-Should::Matchers.configure do |config|
+Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
